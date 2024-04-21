6f0ed8388f0a2122a5cc468c9a85224d2e640329
Adesh kumar chaturvedi <adeshchaturvedi51@gmail.com>
2024-04-21 11:58:58 +0530

    first commit

--- frontend/src/components/food-display/FoodDisplay.jsx
+++ frontend/src/components/food-display/FoodDisplay.jsx
@@ -5,7 +5,9 @@ import { StoreContext } from '../../context/StoreContext'
 const FoodDisplay = () => {
     const {food_list}=useContext(StoreContext)
   return (
-    <div>FoodDisplay</div>
+    <div className="food-displat" id='food-display'>
+        <h2>top dishes near you</h2>
+    </div>
   )
 }
 
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/header/Header'
 import ExploreMenu from '../../components/explore-menu/ExploreMenu'
+import FoodDisplay from '../../components/food-display/FoodDisplay'
 
 const Home = () => {
   const [category ,setCategory] =useState("All")
@@ -10,6 +11,7 @@ const Home = () => {
     <div>
       <Header/>
       <ExploreMenu category ={category} setCategory ={setCategory}/>
+      <FoodDisplay category={category}/>
     </div>
   )
 }
