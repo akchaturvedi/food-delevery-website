fac81ab2b9930a014f5df586915e27cb12bdfbde
Adesh kumar chaturvedi <adeshchaturvedi51@gmail.com>
2024-05-14 20:36:01 +0530

    create food list component

--- frontend/src/components/food-display/FoodDisplay.jsx
+++ frontend/src/components/food-display/FoodDisplay.jsx
@@ -1,12 +1,24 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../food-item/FoodItem'
 
-const FoodDisplay = ({category}) => {
-    const {food_list}=useContext(StoreContext)
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext)
   return (
-    <div className="food-displat" id='food-display'>
-        <h2>top dishes near you</h2>
+    <div className="food-display" id='food-display'>
+      <h2>top dishes near you</h2>
+
+      <div className="food-disply-list">
+        {
+          food_list.map((item, index) => {
+
+            if (category === 'All' || category === item.category) {
+              return <FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image} />
+            }
+          })
+        }
+      </div>
     </div>
   )
 }
